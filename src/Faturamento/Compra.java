f63d3585b5ed9ddef414918a464357ac9ae35e38
JoseERBWolf <wolf.jose89@gmail.com>
2023-07-16 19:55:21 -0300

    Classe que representa a compra

---
+++ src/Faturamento/Compra.java
@@ -0,0 +1,26 @@
+package Faturamento;
+
+public class Compra implements Comparable<Compra>{
+
+    private String nome;
+    private double valor;
+
+    public Compra(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
+
+}
